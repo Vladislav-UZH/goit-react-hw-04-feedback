0f5b3a430a3cb54ac67106e69ff7da6630a91c49
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-01-26 22:28:34 +0200

    rewrite class App to hook

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,69 +1,77 @@
 import Section from 'components/Section';
 import FeedbackOptions from 'components/FeedbackOptions';
 import Statistics from 'components/Statistics';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import Notification from 'components/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-  countTotalFeedback = () => {
-    const values = Object.values(this.state);
+export const App = () => {
+  const [good, setGood] = useState(2000);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const countTotalFeedback = () => {
+    const values = Object.values({ good, neutral, bad });
     return values.reduce((acc, value) => acc + value, 0);
   };
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     const percentage = 100;
     const formula = (good / total) * percentage;
 
     return Number(formula.toFixed(0));
   };
-  addNewStatistics = option => {
-    this.setState(prevState => {
-      return { [option]: prevState[option] + 1 };
-    });
+  const addNewStatistics = option => {
+    switch (option) {
+      case 'good':
+        setGood(setGood => setGood + 1);
+        break;
+      case 'neutral':
+        setNeutral(setNeutral => setNeutral + 1);
+        break;
+      case 'bad':
+        setBad(prevBad => prevBad + 1);
+        break;
+      default:
+        throw new Error('wrong feedback option');
+    }
   };
-  render() {
-    return (
+  useEffect(() => {});
+
+  return (
+    <div
+      style={{
+        padding: 100,
+        color: '#DBD7D7',
+        backgroundColor: '#22232B',
+      }}
+    >
       <div
         style={{
-          padding: 100,
-          color: '#DBD7D7',
-          backgroundColor: '#22232B',
+          padding: 20,
+          backgroundColor: ' #32343b',
+          borderRadius: 10,
         }}
       >
-        <div
-          style={{
-            padding: 20,
-            backgroundColor: ' #32343b',
-            borderRadius: 10,
-          }}
-        >
-          <Section title="Please leave feedback!">
-            <FeedbackOptions
-              updateStats={this.addNewStatistics}
-              options={['Good', 'Neutral', 'Bad']}
+        <Section title="Please leave feedback!">
+          <FeedbackOptions
+            updateStats={addNewStatistics}
+            options={['Good', 'Neutral', 'Bad']}
+          />
+        </Section>
+        <Section title="Statistics:">
+          {countTotalFeedback() ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
             />
-          </Section>
-          <Section title="Statistics:">
-            {this.countTotalFeedback() ? (
-              <Statistics
-                good={this.state.good}
-                neutral={this.state.neutral}
-                bad={this.state.bad}
-                total={this.countTotalFeedback()}
-                positivePercentage={this.countPositiveFeedbackPercentage()}
-              />
-            ) : (
-              <Notification message="There is no feedback!" />
-            )}
-          </Section>
-        </div>
+          ) : (
+            <Notification message="There is no feedback!" />
+          )}
+        </Section>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,36 +1,29 @@
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import { List, Item, Btn } from './FeedbackOptions.styled';
 
-class FeedbackOptions extends Component {
-  static propTypes = {
-    options: PropTypes.array.isRequired,
-    updateStats: PropTypes.func.isRequired,
-  };
-  static defaultOptions = {
-    options: undefined,
-  };
+const FeedbackOptions = ({ options, updateStats }) => {
+  return (
+    <List>
+      {options.map(option => {
+        return (
+          <Item total={options} key={option}>
+            <Btn
+              onClick={() => {
+                let satOption = option.toLowerCase();
+                return updateStats(satOption);
+              }}
+            >
+              {option}
+            </Btn>
+          </Item>
+        );
+      })}
+    </List>
+  );
+};
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  updateStats: PropTypes.func.isRequired,
+};
 
-  render() {
-    const { options, updateStats } = this.props;
-    return (
-      <List>
-        {options.map(option => {
-          return (
-            <Item total={options} key={option}>
-              <Btn
-                onClick={() => {
-                  let satOption = option.toLowerCase();
-                  return updateStats(satOption);
-                }}
-              >
-                {option}
-              </Btn>
-            </Item>
-          );
-        })}
-      </List>
-    );
-  }
-}
 export default FeedbackOptions;
